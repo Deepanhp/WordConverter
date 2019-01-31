8b6cbeedd1461a76c02b8aaa8bffefb9c73029c7
Deepan Kumar <deepan.kumar@mybataz.com>
2019-01-31 17:37:20 +0530

    Checked processing time and commented out

--- WordConverter.rb
+++ WordConverter.rb
@@ -3,8 +3,8 @@ require 'date'
 class WordConverter
 
   def letter_comb(digits)
-    
-    time_start = Time.now()
+
+    #time_start = Time.now()
     return [] if digits.nil? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
 
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
