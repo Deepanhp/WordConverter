464ea67c2b49095b7a3d873bac8a492569b7e9e3
Deepan Kumar <deepan.kumar@mybataz.com>
2019-01-31 17:36:08 +0530

    added validation for digits

--- WordConverter.rb
+++ WordConverter.rb
@@ -3,6 +3,9 @@ require 'date'
 class WordConverter
 
   def letter_comb(digits)
+    
+    time_start = Time.now()
+    return [] if digits.nil? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
 
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
 
@@ -25,7 +28,7 @@ class WordConverter
 
       next if first_array.length < 2
       second_array = keys[i + 1..total_number]
-      
+
       next if second_array.length < 2
       f_comb = first_array.shift.product(*first_array).map(&:join) 
       # print "\nf_comb"
@@ -51,7 +54,7 @@ class WordConverter
       # print "\n final words \n"
       # print final_words
     end
-    
+
     final_words << (keys.shift.product(*keys).map(&:join) & dictionary[11]).join(", ") 
     # time_end = Time.now()
     # puts "Time #{time_end.to_f - time_start.to_f}"
